ffefcfd39ad50190df3ab383c3a1c672ce624d96
walter broncano <santiago25392@gmail.com>
2021-05-03 17:55:31 -0400

    added a final mainjs file.

--- GetNumbers.js
+++ GetNumbers.js
@@ -14,25 +14,3 @@ for (let j = 2; j < process.argv.length; j++) {
 
 
 
-var number = ["Zero" ,"One", "Two" , "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
-
-var count = 0;
-
-
-//print number
-for (var num = 0; a.length > num ;num++ ){
-
-    for (var getnum = 0; a[num].length > getnum  ;getnum++){
-
-        var get = parseInt(a[num][getnum]);
-        //console.log(number[get]);
-        process.stdout.write(number[get]);
-
-    }
-
-    if(a.length- 1 >num ) {
-        process.stdout.write(",");
-    }
-
-
-}
---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+//by Walter Broncano
+//get user input
+var it = 0;
+var a = [];
+
+for (let j = 2; j < process.argv.length; j++) {
+
+    a[it] = (process.argv[j]);
+    it++;
+
+}
+
+
+
+var number = ["Zero" ,"One", "Two" , "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+var count = 0;
+
+
+//print number
+for (var num = 0; a.length > num ;num++ ){
+
+    for (var getnum = 0; a[num].length > getnum  ;getnum++){
+
+        var get = parseInt(a[num][getnum]);
+        //console.log(number[get]);
+        process.stdout.write(number[get]);
+
+    }
+
+    if(a.length- 1 >num ) {
+        process.stdout.write(",");
+    }
+
+
+}
